ab6ca39d33f08e68f556546e8e5d7680e46ffbf5
Kathrin92 <kathrinulmer@gmx.de>
2020-04-20 19:32:37 +0200

    Restructure button component

--- client/src/stories/Button.stories.js
+++ client/src/stories/Button.stories.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import Button from '../components/Button';
-import Button2 from '../components/Button2';
 
 export default {
   title: 'Button',
+  component: Button,
 };
 
 export const LoginButton = () => (
@@ -14,5 +14,5 @@ export const SignUpButton = () => (
   <Button onClick={action('clicked')}>Konto anlegen</Button>
 );
 export const CreateButton = () => (
-  <Button2 onClick={action('clicked')}>Los geht&#39;s!</Button2>
+  <Button onClick={action('clicked')}>Los geht&#39;s!</Button>
 );
