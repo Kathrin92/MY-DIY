599d46a612cceceb833572f8354dad7028fcae24
Kathrin92 <kathrinulmer@gmx.de>
2020-04-21 19:53:06 +0200

    Modify input component

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,15 +1,21 @@
 import styled from '@emotion/styled';
 
 const Input = styled.input`
+  display: flex;
+  justify-content: center;
   text-align: center;
   background: white;
-  color: white;
-  border: 1px #707070 solid;
-  border-radius: 30px;
+  color: #a2a2a1;
+  border: 1px #a2a2a1 solid;
+  border-radius: 10px;
   box-shadow: 0px 3px 6px grey;
   font-size: 1.2rem;
   padding: 0px 0px;
   outline: none;
+  &:hover {
+    color: #ffffff;
+    border: 3px #a2a2a1 solid;
+  }
   cursor: pointer;
   &::placeholder {
     text-align: center;
---
+++ client/src/components/InputDropdown.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+import Input from './Input';
+
+const InputDropdown = styled(Input)`
+  padding: 2px 8px;
+  height: 25px;
+  background-color: #ffffff;
+`;
+
+export default InputDropdown;
