7423ab3427f0b2f6db1914f9ca84be1dd634dc59
Kathrin92 <kathrinulmer@gmx.de>
2020-04-22 17:30:17 +0200

    Add icons and create footer component

---
+++ client/src/stories/Footer.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Footer from '../components/Footer';
+
+export default {
+  title: 'Footer',
+  component: 'Footer',
+};
+
+export const FooterNavigaton = () => <Footer />;
