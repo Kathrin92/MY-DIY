05365e1097c8cc8ad8da50b011d15d4e27b0dc96
Kathrin92 <kathrinulmer@gmx.de>
2020-04-24 15:09:50 +0200

    Install react-query

---
+++ server.js
@@ -0,0 +1,15 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+  })
+);
+server.use(middlewares);
+server.use(router);
+server.listen(4000, () => {
+  console.log('JSON Server is running');
+});
