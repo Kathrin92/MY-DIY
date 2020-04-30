b8c47f7f057f865987b834db388d66bc7dca23b8
Kathrin92 <kathrinulmer@gmx.de>
2020-04-30 21:08:21 +0200

    Add components for input and form

---
+++ client/src/components/InputHeading.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+const InputCaption = styled.span`
+  color: #0e0f09;
+  margin: 10px 0px;
+  font-size: 1.2rem;
+  font-weight: 400;
+`;
+
+export default InputCaption;
