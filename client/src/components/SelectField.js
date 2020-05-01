48fa384ac15a4fc7ee0911a1609ce23686507c83
Kathrin92 <kathrinulmer@gmx.de>
2020-05-01 20:20:45 +0200

    Change select field and related storie

--- client/src/components/SelectField.js
+++ client/src/components/SelectField.js
@@ -2,31 +2,44 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 const Select = styled.select`
-  width: 281px;
-  height: 35px;
-  background-color: #ffffff;
+  background: white;
+  color: #a2a2a1;
+  border: 1px #a2a2a1 solid;
   border-radius: 10px;
-  color: #0e0f09;
+  box-shadow: 0px 3px 6px grey;
+  font-size: 1.1rem;
+  padding: 5px 5px;
+  margin: 10px;
   outline: none;
-  border: none;
-  font-size: 10px;
-  text-align: left;
-  padding: 5px;
-  margin: 5px;
-  margin-bottom: 10px;
+  &:hover {
+    color: #a2a2a1;
+    border: 3px #a2a2a1 solid;
+  }
+  cursor: pointer;
+  &::placeholder {
+    text-align: center;
+    color: #b7b7b7;
+  }
+  &:focus {
+    background-color: #ffffff;
+    color: #0e0f09;
+  }
+`;
+
+const option = styled.option`
+  text-align: center;
 `;
 
 export default function SelectField() {
   return (
     <Select class="ui dropdown">
+      <option>Projekttyp</option>
       <option>Dekoration</option>
       <option>Holzarbeiten</option>
       <option>Malerarbeiten</option>
       <option>Restauration</option>
       <option>Upsycling</option>
-      <option>X</option>
-      <option>X</option>
-      <option>X</option>
+      <option>Sonstiges</option>
     </Select>
   );
 }
