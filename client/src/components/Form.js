e03676d67f075fefc7c0c75a9feb129c8fa3478e
Kathrin Ulmer <60884247+Kathrin92@users.noreply.github.com>
2020-05-03 21:25:25 +0200

    Create selection page. (#44)
    * Add route to selection page
    
    * Link login page and selection page
    
    * Create selection page
    
    * Add components for input and form
    
    * Set link from signup to selection page and delete typo
    
    * Add wrapper for the format and delete inputheading
    
    * Adapt input type

---
+++ client/src/components/Form.js
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const Form = styled.form`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: auto;
+`;
+
+export default Form;
---
+++ client/src/pages/Selection.js
@@ -0,0 +1,41 @@
+import React from 'react';
+//import InputCircle from '../components/InputCircle';
+import InputLine from '../components/InputLine';
+import InputContainer from '../components/InputContainer';
+import Header from '../components/Header';
+import PageHeading from '../components/PageHeading';
+import Button from '../components/Button';
+import ButtonContainer from '../components/ButtonContainer';
+import Form from '../components/Form';
+import Wrapper from '../components/Wrapper';
+import PageContainer from '../components/PageContainer';
+
+function Selection() {
+  return (
+    <PageContainer>
+      <Wrapper>
+        <Header />
+        <PageHeading>Dein neues Projekt</PageHeading>
+      </Wrapper>
+      <Form>
+        <InputContainer>
+          <InputLine type="text" placeholder="Projektname" />
+        </InputContainer>
+        <InputContainer>
+          <InputLine type="text" placeholder="Projekttyp" />
+        </InputContainer>
+        <InputContainer>
+          <InputLine type="text" placeholder="Projekttage" />
+        </InputContainer>
+        <InputContainer>
+          <InputLine type="text" placeholder="Projektschritte" />
+        </InputContainer>
+      </Form>
+      <ButtonContainer>
+        <Button>Los geht&#39;s!</Button>
+      </ButtonContainer>
+    </PageContainer>
+  );
+}
+
+export default Selection;
