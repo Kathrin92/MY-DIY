89457774d2f22848ff8fa33bf1e604d03da10fc8
Kathrin92 <kathrinulmer@gmx.de>
2020-05-06 12:52:47 +0200

    Set project routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import GlobalStyle from './GlobalStyle';
 import Login from '../src/pages/Login';
 import Signup from '../src/pages/Signup';
@@ -11,13 +16,22 @@ function App() {
       <GlobalStyle />
       <Switch>
         <Route exact path="/">
+          <Redirect to="/login" />
+        </Route>
+        <Route exact path="/login">
           <Login />
         </Route>
         <Route exact path="/signup">
           <Signup />
         </Route>
-        <Route exact path="/selection">
+        <Route exact path="/projects/new">
           <Selection />
+        </Route>
+        <Route exact path="/projects">
+          <div>Alle Projekte</div>
+        </Route>
+        <Route exact path="/projects/:projectId">
+          <div>Bestimmtes Projekt</div>
         </Route>
       </Switch>
     </Router>
